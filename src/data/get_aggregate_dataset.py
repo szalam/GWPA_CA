8f4dc928d97e33d04da041438a021c47bb24bc2b
sarfaraz alam <sarfaraz.wre@gmail.com>
2023-03-03 12:53:35 -0800

    update thickness data

---
+++ src/data/get_aggregate_dataset.py
@@ -0,0 +1,261 @@
+#%%
+import os
+import sys
+sys.path.insert(0,'src')
+import config
+import pandas as pd
+
+# list of all csv files
+csv_files_ucd = [f'{config.data_processed}/well_stats/ucdnitrate_stats.csv', 
+            f'{config.data_processed}/gwdepth_wellbuff/GWDepth_wellsrc_UCD_rad_2mile.csv', 
+            f'{config.data_processed}/sagbi_values/SAGBI_wellsrc_UCD_rad_2mile.csv', 
+            f'{config.data_processed}/cafo_pop_wellbuffer/Cafopop_wellsrc_UCD_rad_2mile.csv', 
+            f'{config.data_processed}/cafo_pop_wellbuffer/Cafopop_wellsrc_UCD_rad_5mile.csv', 
+            f'{config.data_processed}/aem_values/AEMsrc_DWR_wellsrc_UCD_rad_2mile_lyrs_1.csv',
+            f'{config.data_processed}/aem_values/AEMsrc_DWR_wellsrc_UCD_rad_2mile_lyrs_4.csv',
+            f'{config.data_processed}/aem_values/AEMsrc_DWR_wellsrc_UCD_rad_2mile_lyrs_6.csv',
+            f'{config.data_processed}/aem_values/AEMsrc_DWR_wellsrc_UCD_rad_2mile_lyrs_9.csv',
+            f'{config.data_processed}/aem_values/AEMsrc_DWR_wellsrc_UCD_rad_2mile_layerwide.csv']
+
+csv_files_gama = [f'{config.data_processed}/well_stats/gamanitrate_stats.csv', 
+            f'{config.data_processed}/gwdepth_wellbuff/GWDepth_wellsrc_GAMA_rad_2mile.csv', 
+            f'{config.data_processed}/sagbi_values/SAGBI_wellsrc_GAMA_rad_2mile.csv', 
+            f'{config.data_processed}/cafo_pop_wellbuffer/Cafopop_wellsrc_GAMA_rad_2mile.csv',
+            f'{config.data_processed}/cafo_pop_wellbuffer/Cafopop_wellsrc_GAMA_rad_5mile.csv', 
+            f'{config.data_processed}/aem_values/AEMsrc_DWR_wellsrc_GAMA_rad_2mile_lyrs_1.csv',
+            f'{config.data_processed}/aem_values/AEMsrc_DWR_wellsrc_GAMA_rad_2mile_lyrs_4.csv',
+            f'{config.data_processed}/aem_values/AEMsrc_DWR_wellsrc_GAMA_rad_2mile_lyrs_6.csv',
+            f'{config.data_processed}/aem_values/AEMsrc_DWR_wellsrc_GAMA_rad_2mile_lyrs_9.csv',
+            f'{config.data_processed}/aem_values/AEMsrc_DWR_wellsrc_GAMA_rad_2mile_layerwide.csv']
+
+def get_combined_dataset(csv_files):
+    # read the first csv file
+    df = pd.read_csv(csv_files[0])
+
+    # rename column 'WELL ID' to 'well_id' if it exists
+    if 'WELL ID' in df.columns:
+        df.rename(columns={'WELL ID': 'well_id'}, inplace=True)
+
+    # remove duplicate based on well_id
+    df = df.drop_duplicates(subset='well_id', keep='first')
+
+    # Iterate over the rest of the csv files
+    for csv_file in csv_files[1:]:
+        df2 = pd.read_csv(csv_file)
+        # rename column 'WELL ID' to 'well_id' if it exists
+        if 'WELL ID' in df2.columns:
+            df2.rename(columns={'WELL ID': 'well_id'}, inplace=True)
+
+        # remove duplicate based on well_id
+        df2 = df2.drop_duplicates(subset='well_id', keep='first')
+
+        # merge the dataframe with the current csv file based on well_id
+        df = pd.merge(df, df2, on='well_id', how='outer')
+
+    return(df)
+
+df_ucd = get_combined_dataset(csv_files_ucd)
+df_ucd['well_data_source'] = 'UCD'
+df_gama = get_combined_dataset(csv_files_gama)
+df_gama['well_data_source'] = 'GAMA'
+
+# Combine two dataset
+df = pd.concat([df_ucd, df_gama], axis=0)
+
+columns_to_keep = ['well_id', 'APPROXIMATE LATITUDE',
+       'APPROXIMATE LONGITUDE', 'mean_nitrate', 'median_nitrate',
+       'max_nitrate', 'min_nitrate', 'measurement_count',
+       'mean_concentration_2015-2022', 'mean_concentration_2010-2015',
+       'mean_concentration_2005-2010', 'mean_concentration_2000-2005',
+       'mean_concentration_2000-2022', 'mean_concentration_2010-2022',
+       'mean_concentration_2007-2009', 'mean_concentration_2012-2015',
+       'mean_concentration_2019-2021', 'mean_concentration_2017-2018',
+       'trend','change_per_year', 'start_date', 'end_date', 
+       'gwdep', 'area_wt_sagbi', 'total_obs', 'well_type',
+       'CAFO_Population_2miles','CAFO_Population_5miles', 'Conductivity_lyrs_9',
+       'Conductivity_lyrs_6','Conductivity_lyrs_4','Conductivity_lyrs_1','well_data_source']
+
+for i in range(1,21):
+    column_name = f'Conductivity_depthwtd_lyr{i}'
+    columns_to_keep.append(column_name)
+
+df = df[columns_to_keep]
+
+# Inverse conductivity to get depth average resistivity
+df['Resistivity_lyrs_9'] = 1/df['Conductivity_lyrs_9']
+df['Resistivity_lyrs_6'] = 1/df['Conductivity_lyrs_6']
+df['Resistivity_lyrs_4'] = 1/df['Conductivity_lyrs_4']
+df['Resistivity_lyrs_1'] = 1/df['Conductivity_lyrs_1']
+#%%
+
+# df = df[['well_id', 'APPROXIMATE LATITUDE',
+#        'APPROXIMATE LONGITUDE', 'mean_nitrate', 'median_nitrate',
+#        'max_nitrate', 'min_nitrate', 'measurement_count',
+#        'mean_concentration_2015-2022', 'mean_concentration_2010-2015',
+#        'mean_concentration_2005-2010', 'mean_concentration_2000-2005',
+#        'mean_concentration_2000-2022', 'mean_concentration_2010-2022',
+#        'mean_concentration_2007-2009', 'mean_concentration_2012-2015',
+#        'mean_concentration_2019-2021', 'mean_concentration_2017-2018',
+#        'trend','change_per_year', 'start_date', 'end_date', 
+#        'gwdep', 'area_wt_sagbi',
+#        'Cafo_Population_2miles','Cafo_Population_5miles', 'Conductivity','well_data_source']]
+
+
+#%%
+# Check for duplicates in the 'well_id' column
+duplicates = df[df.duplicated(['well_id'])]['well_id']
+
+# Print the duplicate values
+if len(duplicates) > 0:
+    print("Duplicate values in 'well_id' column:")
+    # print(duplicates.tolist())
+else:
+    print("No duplicate values in 'well_id' column.")
+
+
+
+#%%
+# Preprocessing the land use data
+# Define the path to the UCD folder
+ucd_folder = f'{config.data_processed}/CDL/cdl_at_buffers/UCD/'
+gama_folder = f'{config.data_processed}/CDL/cdl_at_buffers/GAMA/'
+
+# Create an empty list to store the dataframes
+ucd_dfs = []
+gama_dfs = []
+
+# Iterate through the files in the UCD folder
+for filename in os.listdir(ucd_folder):
+    # Check if the file is a CSV
+    if filename.endswith('.csv'):
+        # Read the CSV into a dataframe
+        df_cdl = pd.read_csv(ucd_folder + filename)
+        df_cdl = df_cdl.drop(columns=df_cdl.columns[0])
+        
+        year = filename.split(".")[0].split("_")[1]
+        
+        # Adding all ag area
+        df_cdl['total_ag'] = df_cdl[['Row_crops', 'Small_grains', 'Truck_nursery_berry', 'Citrus_subtropical', 'Field_crops', 'Vineyards', 'Grain_hay', 'Deciduous_fruits_nuts', 'Rice', 'Cotton', 'Other_crops']].sum(axis=1)
+
+        # Rename the columns of the dataframe
+        df_cdl.columns = [col + '_'+ year if col != 'well_id' else col for col in df_cdl.columns]
+        # Append the dataframe to the list
+        ucd_dfs.append(df_cdl)
+#%%
+# Iterate through the files in the GAMA folder
+for filename in os.listdir(gama_folder):
+    # Check if the file is a CSV
+    if filename.endswith('.csv'):
+        # Read the CSV into a dataframe
+        df_cdl = pd.read_csv(gama_folder + filename)
+        df_cdl = df_cdl.drop(columns=df_cdl.columns[0])
+        # Adding all ag area
+        df_cdl['total_ag'] = df_cdl[['Row_crops', 'Small_grains', 'Truck_nursery_berry', 'Citrus_subtropical', 'Field_crops', 'Vineyards', 'Grain_hay', 'Deciduous_fruits_nuts', 'Rice', 'Cotton', 'Other_crops']].sum(axis=1)
+        
+        year = filename.split(".")[0].split("_")[1]
+        # Rename the columns of the dataframe
+        df_cdl.columns = [col + '_'+ year if col != 'well_id' else col for col in df_cdl.columns]
+        # Append the dataframe to the list
+        gama_dfs.append(df_cdl)
+
+#%%
+# Merging the list items, which are dataframes
+def merge_list_dfs(list_dfs = ucd_dfs):
+    merged_df = list_dfs[0]
+
+    for i in range(1,len(ucd_dfs)):
+        merged_df = pd.merge(merged_df, list_dfs[i], on='well_id')
+    return merged_df
+
+# Concatenate all the dataframes in the list on the 'well_id' column
+ucd_final_df = merge_list_dfs(list_dfs = ucd_dfs)
+gama_final_df = merge_list_dfs(list_dfs = gama_dfs)
+#%%
+print(f'Shape if ucd_final df: {ucd_final_df.shape[0]}, \nwhile total unique wells: {len(ucd_final_df.well_id.unique())}')
+
+
+#%%
+df_cdl_all = pd.concat([ucd_final_df, gama_final_df], axis=0)
+
+df_cdl_all['Average_ag_area'] = df_cdl_all[['total_ag_2007','total_ag_2008','total_ag_2009','total_ag_2010','total_ag_2011','total_ag_2012','total_ag_2013','total_ag_2014','total_ag_2015','total_ag_2016','total_ag_2017','total_ag_2018','total_ag_2019','total_ag_2020','total_ag_2021']].mean(axis=1)
+#%%
+# Fill all NaN with 0
+df_cdl_all = df_cdl_all.fillna(0)
+
+# merge the dataframe with the current csv file based on well_id
+df = pd.merge(df, df_cdl_all, on='well_id', how='outer')
+
+# Merge city in/out information
+df_city_inout = pd.read_csv(config.data_processed / "well_inout_city/well_inout_city.csv")
+#%%
+# merge the dataframe with the current csv file based on well_id
+df = pd.merge(df, df_city_inout, on='well_id', how='outer')
+
+# Merge subregion information information
+df_subreg = pd.read_csv(config.data_processed / "well_in_subregions.csv")
+# merge the dataframe with the current csv file based on well_id
+df = pd.merge(df, df_subreg, on='well_id', how='outer')
+
+# Merge gwpa information
+df_gwpa = pd.read_csv(config.data_processed / "well_in_gwpa.csv")
+# merge the dataframe with the current csv file based on well_id
+df = pd.merge(df, df_gwpa, on='well_id', how='outer')
+
+# Read and merge the thickness for different conductivity threshold
+
+def get_thickness_data(df, aem_lyr_lim = 4, dtype_sel = 'GAMA',rad_buffer = 2):
+    # Set the values of cond_thresh to run the code for
+    cond_thresh_values = [0.05, 0.08, 0.10, 0.15]
+    # Loop over the cond_thresh values and run the code for each value
+    for cond_thresh in cond_thresh_values:
+        # Read the DataFrame from the CSV file
+        df_thickness = pd.read_csv(config.data_processed / f"aem_values/Thickness_abovThresh_DWR_wellsrc_{dtype_sel}_rad_{rad_buffer}mile_lyrs_{aem_lyr_lim}_condThresh_{round(cond_thresh*100)}.csv")
+        # Check if the column exists
+        old_col_name = 'Conductivity_lyrs_9'
+        old_col_name2 = f'thickness_abovCond_{round(cond_thresh*100)}'
+        new_col_name = f'thickness_abovCond_{round(cond_thresh*100)}_lyrs_{aem_lyr_lim}_rad_{rad_buffer}miles'
+        if old_col_name in df_thickness.columns:
+            # Rename the column to the new name
+            df_thickness = df_thickness.rename(columns={old_col_name: new_col_name})
+        if old_col_name2 in df_thickness.columns:
+            # Rename the column to the new name
+            df_thickness = df_thickness.rename(columns={old_col_name2: new_col_name})
+
+        # Select the columns we want to keep
+        df_thickness = df_thickness[['well_id', new_col_name]]
+
+        # Merge with the main DataFrame
+        df = pd.merge(df, df_thickness, on='well_id', how='outer')
+
+    return df
+
+# df_tmp = df[['well_id']]
+# df_gama_thickness_4 = get_thickness_data(df = df_tmp,aem_lyr_lim = 4, dtype_sel = 'GAMA',rad_buffer = 2)
+# df_gama_thickness_6 = get_thickness_data(df = df_tmp,aem_lyr_lim = 6, dtype_sel = 'GAMA')
+# df_gama_thickness_9 = get_thickness_data(df = df_tmp,aem_lyr_lim = 9, dtype_sel = 'GAMA')
+# df_ucd_thickness_4 = get_thickness_data(df = df_tmp,aem_lyr_lim = 4, dtype_sel = 'UCD')
+# df_ucd_thickness_6 = get_thickness_data(df = df_tmp,aem_lyr_lim = 6, dtype_sel = 'UCD')
+# df_ucd_thickness_9 = get_thickness_data(df = df_tmp,aem_lyr_lim = 9, dtype_sel = 'UCD')
+# # Combine two dataset
+# df_thickness = pd.concat([df_gama_thickness_4, df_gama_thickness_6,df_gama_thickness_9,
+#                           df_ucd_thickness_4,df_ucd_thickness_6,df_ucd_thickness_9], axis=0)
+
+df_tmp = df[['well_id']]
+df_thickness_list = []
+
+for aem_lyr_lim in [4, 6, 9]:
+    for rad_buffer in [.5, 1, 2, 3, 4, 5]:
+        df_gama_thickness = get_thickness_data(df=df_tmp, aem_lyr_lim=aem_lyr_lim, dtype_sel='GAMA', rad_buffer=rad_buffer)
+        df_thickness_list.append(df_gama_thickness)
+
+df_thickness = pd.concat(df_thickness_list)
+df = pd.merge(df, df_thickness, on='well_id', how='outer')
+#%%
+# Use the str.replace() method to remove 'NO3_' from all values in the 'well_id' column
+df['well_id'] = df['well_id'].str.replace('NO3_', '')
+
+# export final dataframe to csv
+df.to_csv(config.data_processed / "Dataset_processed.csv", index=False)
+
+# %%
+ 
---
+++ src/data/get_thickness_wellbuffer_multiregions.py
@@ -0,0 +1,180 @@
+#========================================================================================
+# This script calculates the thickness of well buffer above a threhold conductivity value within well buffers.
+#========================================================================================
+#%%
+import sys
+sys.path.insert(0,'src')
+import config
+import fiona
+import warnings
+import pandas as pd
+import ppfun as dp
+import geopandas as gpd
+import numpy as np
+from shapely.geometry import Point
+from geopandas import GeoDataFrame
+from pyproj import CRS
+from tqdm import tqdm
+from contextlib import redirect_stdout
+
+def get_thickness_well_buffer(well_src = 'UCD',cond_thresh= 0.15, aem_lyr_lim = 9,rad_buffer = 2):
+    # file location
+    file_polut = config.data_raw / "nitrate_data/UCDNitrateData.csv"
+    file_aem = config.data_processed / 'AEM'
+
+    #==================== User Input requred ==========================================
+    aem_src        = 'DWR'          # options: DWR, ENVGP
+    # well_src       = 'UCD'          # options: UCD, GAMA
+    aem_regions = [4, 5, 6, 7] # list of all regions
+    aem_value_type = 'conductivity' # options: resistivity, conductivity
+    aem_stat       = 'mean'         # options: mean, min
+    # rad_buffer     = 2              # well buffer radius in miles
+    buffer_flag    = 0              # flag 1: use existing buffer shapefile; 0: create buffer
+    # cond_thresh    = 0.15           # [0.01, 0.02, 0.03, 0.05, 0.07, 0.08, 0.1, 0.15, 0.18, 0.2]
+    # aem_lyr_lim    = 9              # options: 9, 8. For DWR use 9,3,20, for ENVGP use 8
+
+    #==================================================================================
+
+
+    #=========================== Import water quality data ==========================
+    if well_src == 'GAMA':
+        file_polut = config.data_gama_all / 'CENTRALVALLEY_NO3N_GAMA.csv'
+        df= dp.get_polut_df(file_sel = file_polut)
+        df.rename(columns = {'GM_WELL_ID':'WELL ID', 'GM_LATITUDE':'APPROXIMATE LATITUDE', 'GM_LONGITUDE':'APPROXIMATE LONGITUDE', 'GM_CHEMICAL_VVL': 'CHEMICAL', 'GM_RESULT': 'RESULT','GM_WELL_CATEGORY':'DATASET_CAT','GM_SAMP_COLLECTION_DATE':'DATE'}, inplace = True)
+        df['DATE']= pd.to_datetime(df['DATE'])
+
+    if well_src == 'UCD':
+        # file location
+        file_polut = config.data_raw / "nitrate_data/UCDNitrateData.csv"
+
+        # Read nitrate data
+        df = dp.get_polut_df(file_sel = file_polut)
+
+    # Group the DataFrame by the 'WELL ID' column and take the first values for other selected columns
+    df = df.groupby('WELL ID')['APPROXIMATE LATITUDE', 'APPROXIMATE LONGITUDE',].apply(lambda x: x.iloc[0]).reset_index()
+
+    #============================ Import AEM data ===============================
+    thick_loc = 'thickness_abov_threshold_cond'
+    def aem_info(file_aem, aem_src, aem_value_type, aem_lyr_lim, aem_reg,cond_thresh):
+        aem_fil_loc = file_aem / aem_src / aem_value_type / thick_loc
+        if aem_src == 'DWR':
+            interpolated_aem_file = f'lyr_thickness_above_threshold_lyrs_{aem_lyr_lim}_cond_{round(cond_thresh*100)}_reg{aem_reg}.npy'
+        elif aem_src == 'ENVGP':
+            interpolated_aem_file = f'lyr_thickness_above_threshold_lyrs_{aem_lyr_lim}_cond_{round(cond_thresh*100)}_reg{aem_reg}.npy'
+        return aem_fil_loc, interpolated_aem_file
+   
+    # # call the function
+    # aem_fil_loc1, interpolated_aem_file1 = aem_info(file_aem, aem_src, aem_value_type, aem_lyr_lim, aem_reg)
+    # aem_fil_loc2, interpolated_aem_file2 = aem_info(file_aem, aem_src, aem_value_type, aem_lyr_lim, aem_reg2)
+
+
+    # Create a list of arguments for the get_aem_from_npy function
+    aem_args = []
+    for aem_region in aem_regions:
+        aem_fil_loc, interpolated_aem_file = aem_info(file_aem, aem_src, aem_value_type, aem_lyr_lim, aem_region,cond_thresh)
+        aem_args.append({
+            'file_loc_interpolated': aem_fil_loc, 
+            'file_aem_interpolated': interpolated_aem_file, 
+            'aemregion': aem_region, 
+            'aemsrc': aem_src
+        })
+
+    
+    # Use a list comprehension to apply the get_aem_from_npy function to each set of arguments
+    gdf_aem_list = [dp.get_aem_from_npy(**args) for args in aem_args]
+
+    # Concatenate the dataframes into one
+    gdfaem = pd.concat(gdf_aem_list)
+
+    #======================== Get boundary of AEM data ========================
+    # converting point resistivity data to polygon mask
+    gdftmp = gdfaem.copy()
+    gdftmp=gdftmp.dropna(subset=['Resistivity'])
+
+    gdftmp['id_1'] = 1
+    gdfmask = gdftmp.dissolve(by="id_1")
+    gdfmask["geometry"] = gdfmask["geometry"].convex_hull
+
+    gdfaem_boundary = gdfmask
+
+    
+    if buffer_flag == 1:
+        gdf_wellbuffer = pd.read_pickle(config.data_processed / f'Well_buffer_shape' / f"{well_src}_buffers_{(rad_buffer)}mile.pkl")
+
+    if buffer_flag != 1:
+        # Creating buffer around wells
+        gdf_wellbuffer_all = dp.get_well_buffer_shape(df,rad_buffer = rad_buffer) 
+
+        # Perform  clip on the well data based on selected boundary
+        gdf_wellbuffer = gpd.sjoin(gdf_wellbuffer_all, gdfaem_boundary, how='inner', op='within')
+        gdf_wellbuffer = gdf_wellbuffer[['well_id', 'lat', 'lon', 'geometry']]
+
+        # Export the buffer to a shapefile
+        # gdf_wellbuffer.to_pickle(config.data_processed / f'Well_buffer_shape' / f"{well_src}_buffers_{(rad_buffer)}mile.pkl")
+        gdf_wellbuffer.to_file(config.data_processed / f'Well_buffer_shape' / f"{well_src}_buffers_{(rad_buffer)}mile.shp", driver='ESRI Shapefile')
+
+
+    #============================ AEM values in the well buffer ===============================
+
+    def get_aem_mean_in_well_buffer(gdfres, wqnodes_2m_gpd, aem_value_type):
+        """
+        Get the mean resistivity of AEM data in the buffer around each well.
+
+        Parameters:
+        - gdfres (gpd.GeoDataFrame): AEM data
+        - wqnodes_2m_gpd (gpd.GeoDataFrame): Buffer around each well
+        """
+        # Make a copy of the AEM data
+        aemdata = gdfres.copy()
+        
+        # Get the intersections between the AEM data and the well buffer
+        aem_wq_buff = gpd.overlay(aemdata, wqnodes_2m_gpd, how='intersection')
+        
+        # Compute the mean resistivity for each well
+        aem_wq_buff_aemmean = aem_wq_buff.groupby("well_id").Resistivity.apply(lambda x: np.nanmean(x)).reset_index(name='Resistivity')
+        
+        # Merge the mean resistivity data with the well buffer data
+        aem_wq_buff_aemmean = aem_wq_buff_aemmean.merge(wqnodes_2m_gpd, on='well_id', how='left')
+        
+        # Select few columns
+        aem_wq_buff_aemmean = aem_wq_buff_aemmean[['well_id', 'Resistivity', 'lat', 'lon', 'geometry']]
+
+        if aem_value_type == 'conductivity':
+            aem_wq_buff_aemmean = aem_wq_buff_aemmean.rename(columns={'Resistivity': f'thickness_abovCond_{round(cond_thresh*100)}'}) 
+
+        return aem_wq_buff_aemmean
+
+    warnings.filterwarnings("ignore")
+
+    # Get AEM values inside buffer
+    aem_inside_buffer = get_aem_mean_in_well_buffer(gdfres= gdfaem, wqnodes_2m_gpd = gdf_wellbuffer,aem_value_type = aem_value_type)
+
+    # Drop geometry column
+    aem_inside_buffer2 = aem_inside_buffer.drop(['geometry'], axis=1)
+
+    # Export CSV with AEM values
+    aem_inside_buffer2.to_csv(config.data_processed / f"aem_values/Thickness_abovThresh_{aem_src}_wellsrc_{well_src}_rad_{rad_buffer}mile_lyrs_{aem_lyr_lim}_condThresh_{round(cond_thresh*100)}.csv")
+#%%
+well_rad_all = [0.5,1,2,3,4,5]
+cond_thresh_values = [0.05, 0.057, 0.066, 0.08, 0.1, 0.15]
+aem_lyr_lim_values = [4, 6, 9]
+well_src_values = ['UCD', 'GAMA']
+
+for well_src in well_src_values:
+    for well_rad_sel in well_rad_all:
+        for cond_thresh in cond_thresh_values:
+            for aem_lyr_lim in aem_lyr_lim_values:
+                get_thickness_well_buffer(well_src=well_src, cond_thresh=cond_thresh, aem_lyr_lim=aem_lyr_lim,rad_buffer = well_rad_sel)
+
+#%%
+#============================ Exporting data to kml ==================================
+# fiona.supported_drivers['KML'] = 'rw'
+
+# gdf_wellbuffer2 = gdf_wellbuffer.copy()
+# gdf_wellbuffer2 = gdf_wellbuffer2[['well_id', 'geometry']]
+
+# # Define the output file name
+# gdf_wellbuffer2.to_file(config.data_processed / f'Well_buffer_shape' / f'{well_src}_buffer_{rad_buffer}mile.kml', driver='KML')
+
+# %%
+
